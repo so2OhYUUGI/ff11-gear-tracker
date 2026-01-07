bf2b192e48578daa97f5d5a35761d22faea2c53d
so2 <so2jin5@gmail.com>
2026-01-07 11:20:29 +0900

    データベースの整合性を調整

---
+++ doc/supabase/20260106_insert_initdata.sql
@@ -0,0 +1,8 @@
+INSERT INTO items (id, name, category) VALUES
+  ('alexandrite', 'アレキサンドライト', 'RMEA'),
+  ('heavy_metal', 'ヘヴィメタル', 'RMEA'),
+  ('riftdross', 'リフトドロス', 'RMEA'),
+  ('riftcinder', 'リフトシンダー', 'RMEA'),
+  ('high_purified_anthracite', '高純度ベヤルド', 'RMEA'),
+  ('hallmark', 'ホールマーク', 'Point'),
+  ('gallantry', 'ガラントリー', 'Point');
